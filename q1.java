922e925bc60696302e251975ff4ddba16d79c699
442010485 <91111202+442010485@users.noreply.github.com>
2021-10-12 09:17:54 +0300

    Update q1.java

--- q1.java
+++ q1.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class Q1{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int Num1 = scan.nextInt();
+        Num1 = Num1 %2;
+        
+        if (Num1 == 0){
+        System.out.println("even");           
+        }
+        else{
+            System.out.println("Odd");
+        }}}
 
