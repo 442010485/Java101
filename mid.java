4fe5df9c842debcdc3d1b8374fe3b21374c9813b
442010485 <91111202+442010485@users.noreply.github.com>
2021-10-20 21:00:48 +0300

    Update mid.java

--- mid.java
+++ mid.java
@@ -1,1 +1,20 @@
-
+import java.util.Scanner; 
+public class Mid {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String myname = scan.nextLine();
+        
+   
+    int age= scan.nextInt();
+    age = 2021 - age;
+    
+    
+    if (age>=40)
+    System.out.println();
+    
+    else if  (age>=30 && age<=39 )
+    System.out.println(myname+age);
+    
+    else;
+    System.out.println(myname+age);
+  }}
