6661579802b484df146a09acb6d5b7d4e656997a
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-12 08:12:31 +0300

    Create Stars.java

---
+++ Stars.java
@@ -0,0 +1,11 @@
+public class Stars {
+  public static void main(String [] args){
+     
+       for (int   k = 0 ;   k< 5 ; k ++){
+       System.out.println("");
+       for (int s=0 ; s< 5-k; s ++ ){
+       System.out.print("*");}}
+      
+  }
+    
+}
