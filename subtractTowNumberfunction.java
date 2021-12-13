4042d8205563dcb043908bf7e55dbb7eca28b7b4
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-13 21:39:30 +0300

    Create subtractTowNumberfunction.java

---
+++ subtractTowNumberfunction.java
@@ -0,0 +1,14 @@
+public class Main {
+  static int mymethod(int number1 , int number2 ) {
+    return number1 - number2;
+  }
+  
+ 
+  
+  public static void main(String[] args) {
+    int x = mymethod(15, 13);
+ 
+    System.out.println( x);
+    
+  }
+}
