a100ef7910849ddc0b30b9ebbc35aaa6b51238a9
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-13 20:33:38 +0300

    Create weekendAndweekdays.java

---
+++ weekendAndweekdays.java
@@ -0,0 +1,24 @@
+public class weekendAndweekdays
+{
+	public static void main(String[] args) {
+	 int day =4;
+switch (day) {
+  case 1:
+    
+  case 2:
+   
+  case 3:
+
+  case 4:
+    
+    
+  case 5:
+	System.out.println("weekdays");
+	break ;
+	case 6:
+	    case 7:
+	        System.out.println("weekend");
+      
+}
+}
+}
