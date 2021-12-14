a7e9fa6809853c5294726c3ef7bfc6c6f976ef0a
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-14 21:52:32 +0300

    Create readWordByWord.java

---
+++ readWordByWord.java
@@ -0,0 +1,7 @@
+public class readWordByWord.java  {
+        public static void Main(String[] args) {
+              String mm="moner /n almalki";
+              System.out.print(mm);
+
+        }
+}
