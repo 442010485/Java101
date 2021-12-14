a16bc1126a50a16129e42166fb363002432ca5f3
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-14 21:35:25 +0300

    Create powFunction.java

---
+++ powFunction.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class  powFunction.java {
+    static int add(int moner) {
+        return moner*moner;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int moner;
+        System.out.println("Enter the number");
+        moner= scan.nextInt();
+
+
+        System.out.println(add(moner));
+
+
+
+
+
+
+    }
+}
