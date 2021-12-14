f3e0f83082bbac63d6c4821fdbd082f0c19df393
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-14 12:10:03 +0300

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class largest {
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        float m1;
+        float h1;
+        float r1;
+        System.out.println("Enter Number1");
+        m1 = scan.nextFloat();
+        System.out.println("Enter Number2");
+        h1 = scan.nextInt();
+        System.out.println("Enter Number3");
+        r1 = scan.nextFloat();
+
+        if(m1>h1 && m1>r1 ){
+        System.out.println("The largest score is" + m1);
+        }
+        else if(h1>m1 && h1>r1 ){
+        System.out.println("The largest score is  " + h1);
+        }
+        else if(r1>m1 && r1>h1 ){
+        System.out.println("The largest score is"+r1);
+        }
+        
+        }
+        }
