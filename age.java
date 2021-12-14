c2d06d22d2b4143b18fa1a0e20f3747b09fe00d9
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-14 21:11:54 +0300

    Update age.java

--- age.java
+++ age.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner; 
-public class age {
+public class age.java {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     String myname = scan.nextLine();
