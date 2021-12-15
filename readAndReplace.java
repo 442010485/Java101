5d1130a4f1821098c901b443b65531ef177dee4a
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-15 14:57:27 +0300

    Create readAndReplace.java

---
+++ readAndReplace.java
@@ -0,0 +1,11 @@
+public class readAndReplace.java{
+public static void main(String[] args) {
+     String name = "Moneer";
+     for (int x =0;  x<=8 ; ++x){
+         if(name.charAt(x) == 'e'){
+        System.out.println('x');}
+        else {
+            System.out.println(name.charAt(x));
+        }
+        
+     }}}
