dbe3f3b9f4d66207debe703d77f6913efdc078f4
442010485 <91111202+442010485@users.noreply.github.com>
2021-12-21 08:29:51 +0300

    Create Exam.java

---
+++ Exam.java
@@ -0,0 +1,16 @@
+public class Main{
+	public static void main(String[] args) {
+ 
+ 
+ 	int counter=1;
+	while(counter <= 100){
+	  
+	     if(counter%2==0)
+	    
+	    System.out.println(counter+" %2 is even");
+	    
+	    else
+	          System.out.println(counter+" %2 is odd ");
+	        ++counter;
+	}}}
+	
